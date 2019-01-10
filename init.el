97995d7895cb947ce2dc041dca90b05d93f8f08f
Daichi Takamiya <data0x200@gmail.com>
2019-01-10 14:15:20 +0900

    update

--- init.el
+++ init.el
@@ -578,6 +578,7 @@
   (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.es6$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.js[x]?$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tag$" . web-mode))
   (add-hook 'web-mode-hook (lambda ()
                              (setq web-mode-enable-auto-quoting nil)
                              (setq web-mode-auto-close-style 1)
