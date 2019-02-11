f9974fe1b19d7555ddeab6c6abf0d8033dd8309f
Daichi Takamiya <data0x200@gmail.com>
2019-02-11 14:55:47 +0900

    update

--- init.el
+++ init.el
@@ -299,9 +299,9 @@
 
 ;;;; input method
 (when (linux-p)
-  (require 'mozc)
-  (setq default-input-method "japanese-mozc")
-  (setq mozc-candidate-style 'overlay)
+  (when (require 'mozc nil t)
+    (setq default-input-method "japanese-mozc")
+    (setq mozc-candidate-style 'overlay))
   ;; (add-hook 'ibus-mode-hook
   ;;           (lambda ()
   ;;             (define-key mozc-mode-map (kbd "C-[") 'toggle-input-method)))
