62ca6168e1d3814e55d179280fe152952ecea1a1
Daichi Takamiya <data0x200@gmail.com>
2019-03-05 00:02:33 +0900

    update

--- init.el
+++ init.el
@@ -100,22 +100,7 @@
 ;; daily-memo
 (defun my/memo ()
   (interactive)
-  (find-file-other-window (expand-file-name "~/Dropbox/memo/memo.markdown"))
-  (progn
-    (beginning-of-buffer)
-    (next-line)
-    (insert (concat "\n" "## " (format-time-string "%Y/%m/%d %H:%M:%S") " ##" "\n\n\n"))
-    (previous-line)))
-
-;; daily-memo
-(defun my/work-memo ()
-  (interactive)
-  (find-file-other-window (expand-file-name "~/.memo/memo.markdown"))
-  (progn
-    (beginning-of-buffer)
-    (next-line)
-    (insert (concat "\n" "## " (format-time-string "%Y/%m/%d") " ##" "\n\n\n"))
-    (previous-line)))
+  (find-file-other-window (expand-file-name "~/.memo/memo.org")))
 
 ;;========================================
 ;; General Config
@@ -291,30 +276,14 @@
 ;;;; exec-path-from-shell
 (el-get-bundle! exec-path-from-shell
   (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)
-    (exec-path-from-shell-copy-env "PATH")
-    (exec-path-from-shell-copy-env "LANG")
-    (exec-path-from-shell-copy-env "GOPATH")
-    (exec-path-from-shell-copy-env "GOROOT")))
+    (exec-path-from-shell-initialize)))
 
 ;;;; input method
 (when (linux-p)
   (when (require 'mozc nil t)
     (setq default-input-method "japanese-mozc")
     (setq mozc-candidate-style 'overlay))
-  ;; (add-hook 'ibus-mode-hook
-  ;;           (lambda ()
-  ;;             (define-key mozc-mode-map (kbd "C-[") 'toggle-input-method)))
-
-  ;; (setq default-input-method "japanese-skk-uim")
-  ;; ;; Set UTF-8 as preferred character encoding (default is euc-jp).
-  ;; (setq uim-lang-code-alist
-  ;;       (cons '("Japanese" "Japanese" utf-8 "UTF-8")
-  ;;             (delete (assoc "Japanese" uim-lang-code-alist)
-  ;;                     uim-lang-code-alist)))
-  ;; (setq uim-default-im-prop '("action_skk_hiragana"))
-  (global-set-key (kbd "C-'") 'toggle-input-method)
-  )
+  (global-set-key (kbd "C-'") 'toggle-input-method))
 
 ;;;; theme
 (el-get-bundle hbin/molokai-theme
@@ -343,6 +312,26 @@
   (add-hook 'c-mode-hook 'fci-mode))
 
 ;; Utility
+;;; Org mode
+(define-key ctrl-q-map (kbd "C-m") 'org-capture)
+
+(setq org-capture-templates
+      '(("m"
+         "✍ Memo"
+         entry
+         (file+headline "~/.memo/memo.org" "Memo")
+         "* %U\n%a\n%i\n%?")
+        ("t"
+         "TODO"
+         entry
+         (file+headline "~/.memo/memo.org" "TODOs")
+         "* TODO %?")))
+(setq org-agenda-files '("~/.memo/memo.org"))
+
+(el-get-bundle org-pomodoro
+  (when (require 'org-agenda nil t)
+    (define-key org-agenda-mode-map (kbd "p") 'org-pomodoro)))
+
 ;;;; popwin
 (el-get-bundle! popwin)
 
@@ -662,72 +651,6 @@
            (add-hook 'before-save-hook 'my/clang-format))))
   (add-hook 'before-save-hook 'my/clang-format))
 
-;;;; Gauche
-(setq scheme-program-name "gosh -i")
-(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
-(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
-;; 以下はインデントの定義です。
-(put 'and-let* 'scheme-indent-function 1)
-(put 'begin0 'scheme-indent-function 0)
-(put 'call-with-client-socket 'scheme-indent-function 1)
-(put 'call-with-input-conversion 'scheme-indent-function 1)
-(put 'call-with-input-file 'scheme-indent-function 1)
-(put 'call-with-input-process 'scheme-indent-function 1)
-(put 'call-with-input-string 'scheme-indent-function 1)
-(put 'call-with-iterator 'scheme-indent-function 1)
-(put 'call-with-output-conversion 'scheme-indent-function 1)
-(put 'call-with-output-file 'scheme-indent-function 1)
-(put 'call-with-output-string 'scheme-indent-function 0)
-(put 'call-with-temporary-file 'scheme-indent-function 1)
-(put 'call-with-values 'scheme-indent-function 1)
-(put 'dolist 'scheme-indent-function 1)
-(put 'dotimes 'scheme-indent-function 1)
-(put 'if-match 'scheme-indent-function 2)
-(put 'let*-values 'scheme-indent-function 1)
-(put 'let-args 'scheme-indent-function 2)
-(put 'let-keywords* 'scheme-indent-function 2)
-(put 'let-match 'scheme-indent-function 2)
-(put 'let-optionals* 'scheme-indent-function 2)
-(put 'let-syntax 'scheme-indent-function 1)
-(put 'let-values 'scheme-indent-function 1)
-(put 'let/cc 'scheme-indent-function 1)
-(put 'let1 'scheme-indent-function 2)
-(put 'letrec-syntax 'scheme-indent-function 1)
-(put 'make 'scheme-indent-function 1)
-(put 'multiple-value-bind 'scheme-indent-function 2)
-(put 'match 'scheme-indent-function 1)
-(put 'parameterize 'scheme-indent-function 1)
-(put 'parse-options 'scheme-indent-function 1)
-(put 'receive 'scheme-indent-function 2)
-(put 'rxmatch-case 'scheme-indent-function 1)
-(put 'rxmatch-cond 'scheme-indent-function 0)
-(put 'rxmatch-if  'scheme-indent-function 2)
-(put 'rxmatch-let 'scheme-indent-function 2)
-(put 'syntax-rules 'scheme-indent-function 1)
-(put 'unless 'scheme-indent-function 1)
-(put 'until 'scheme-indent-function 1)
-(put 'when 'scheme-indent-function 1)
-(put 'while 'scheme-indent-function 1)
-(put 'with-builder 'scheme-indent-function 1)
-(put 'with-error-handler 'scheme-indent-function 0)
-(put 'with-error-to-port 'scheme-indent-function 1)
-(put 'with-input-conversion 'scheme-indent-function 1)
-(put 'with-input-from-port 'scheme-indent-function 1)
-(put 'with-input-from-process 'scheme-indent-function 1)
-(put 'with-input-from-string 'scheme-indent-function 1)
-(put 'with-iterator 'scheme-indent-function 1)
-(put 'with-module 'scheme-indent-function 1)
-(put 'with-output-conversion 'scheme-indent-function 1)
-(put 'with-output-to-port 'scheme-indent-function 1)
-(put 'with-output-to-process 'scheme-indent-function 1)
-(put 'with-output-to-string 'scheme-indent-function 1)
-(put 'with-port-locking 'scheme-indent-function 1)
-(put 'with-string-io 'scheme-indent-function 1)
-(put 'with-time-counter 'scheme-indent-function 1)
-(put 'with-signal-handlers 'scheme-indent-function 1)
-(put 'with-locking-mutex 'scheme-indent-function 1)
-(put 'guard 'scheme-indent-function 1)
-
 ;;;; Java
 (defun java-indent-hook()
   (setq indent-tabs-mode nil)
@@ -798,11 +721,8 @@
 ;;;; C-x C-oでウィンドウを切り替える
 (global-set-key (kbd "C-o") 'other-window)
 
-;;;; my memo
-(define-key ctrl-q-map (kbd "C-m") 'my/memo)
-
 ;;;; my work memo
-(define-key ctrl-q-map (kbd "C-w") 'my/work-memo)
+(define-key ctrl-q-map (kbd "C-w") 'my/memo)
 
 (show-paren-mode t)
 
