2fb751501f78b421b44a0b7d27e4e8aa8aa9744f
Daichi Takamiya <data0x200@gmail.com>
2020-02-12 19:39:01 +0900

    update

--- init.el
+++ init.el
@@ -750,6 +750,7 @@
   (add-to-list 'auto-mode-alist '("\\.tag$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.json$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ctp$" . web-mode))
   (add-hook 'web-mode-hook (lambda ()
                              (setq web-mode-enable-auto-quoting nil)
                              (setq web-mode-auto-close-style 1)
